83d83612a0deadd44c5003cbfa236a6c6e288f14
viggotico <victors4ngo@gmail.com>
2024-11-01 17:48:45 +0100

    settings page - victor

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import "../css/SettingsPage.css"; // Make sure your CSS is properly set up
-import profilePicture from "../assets/people/self/pexels-jonaorle-5673896.jpg"; // Ensure the correct image path
+import "../css/SettingsPage.css";
 
 export const SettingsPage = () => {
-  return <></>;
+    return <></>;
 }
